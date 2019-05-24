3c9ff0ef79a8f1d9de0279bc7f2456fd682b82bd
iteradmin <abhi2108jeet@gmail.com>
2019-05-24 16:56:25 +0530

    checking_PM

--- app/src/main/java/com/example/iteradmin/checking/MainActivity.kt
+++ app/src/main/java/com/example/iteradmin/checking/MainActivity.kt
@@ -10,24 +10,51 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
+        val ch1=findViewById<CheckBox>(R.id.checkBox)
+        val ch2=findViewById<CheckBox>(R.id.checkBox2)
+        val ch3=findViewById<CheckBox>(R.id.checkBox3)
+        val ch4=findViewById<CheckBox>(R.id.checkBox4)
           val id:Int=v.id
           when(id){
-              R.id.checkBox ->
-              {
-                 val s:String=findViewById<CheckBox>(R.id.checkBox).text.toString()
-                  toast(s)
-              }
-              R.id.checkBox2 ->{
-                  val s:String=findViewById<CheckBox>(R.id.checkBox2).text.toString()
-                  toast(s)
+               R.id.checkBox ->{
+                   val ch1=findViewById<CheckBox>(R.id.checkBox)
+                   if(ch1.isChecked){
+                       ch2.isChecked=false
+                       ch3.isChecked=false
+                       ch4.isChecked=false
+                       toast(ch1.text.toString())
+                   }
+
                }
+              R.id.checkBox2 ->{
+                  val ch2=findViewById<CheckBox>(R.id.checkBox2)
+                  if(ch2.isChecked){
+                      ch1.isChecked=false
+                      ch3.isChecked=false
+                      ch4.isChecked=false
+                      toast(ch2.text.toString())
+                  }
+
+              }
               R.id.checkBox3 ->{
-                  val s:String=findViewById<CheckBox>(R.id.checkBox3).text.toString()
-                  toast(s)
+                  val ch3=findViewById<CheckBox>(R.id.checkBox3)
+                  if(ch3.isChecked){
+                      ch2.isChecked=false
+                      ch1.isChecked=false
+                      ch4.isChecked=false
+                      toast(ch3.text.toString())
+                  }
+
               }
               R.id.checkBox4 ->{
-                  val s:String=findViewById<CheckBox>(R.id.checkBox4).text.toString()
-                  toast(s)
+                  val ch4=findViewById<CheckBox>(R.id.checkBox4)
+                  if(ch4.isChecked){
+                      ch2.isChecked=false
+                      ch3.isChecked=false
+                      ch1.isChecked=false
+                      toast(ch4.text.toString())
+                  }
+
               }
           }
     }
